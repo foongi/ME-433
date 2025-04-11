538e6c625951de2da6dcc1c33f34941d14b96cab
Ethan Foong <ivethan5@gmail.com>
2025-04-10 20:46:46 -0500

    Finished HW 4 DAC

--- HW 4/SPI_DAC/SPI_DAC.c
+++ HW 4/SPI_DAC/SPI_DAC.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include <math.h>
+
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -10,6 +12,9 @@
 #define PIN_CS   17
 #define PIN_SCK  18
 #define PIN_MOSI 19
+
+static float sine[50];
+static float triangle[100];
 
 
 static inline void cs_select(uint cs_pin) {
@@ -24,27 +29,58 @@ static inline void cs_deselect(uint cs_pin) {
     asm volatile("nop \n nop \n nop"); // FIXME
 }
 
-void writeDAC(int channel, float voltage) {
+void writeDAC(int channel, float duty_cycle) {
+
+    uint bits = (int) (duty_cycle * 1023.0);
+    
+    uint8_t frontbits = ((0b1 & channel) << 7) | (0b111 << 4) | ((0b1111000000 & bits) >> 6);
+    uint8_t backbits = ((0b111111 & bits) << 2);
+
+
     uint8_t data[2];
     int len = 2;
-    data[0] = 0b01111111;
-    data[1] = 0b11111100;
+    data[0] = frontbits;
+    data[1] = backbits;
 
     cs_select(PIN_CS);
     spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
     cs_deselect(PIN_CS);
 
-    data[0] = 0b11110001;
-    data[1] = 0b11111100;
+}
 
-    cs_select(PIN_CS);
-    spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
-    cs_deselect(PIN_CS);
+void generateWaveforms()
+{
+    float radians = 0;
+    for(int i = 0; i < 50; i++)
+    {
+        // 2pi radians in a full sine cycle. 50 points in sine cycle
+        // 0.1256637 is 1/50 of 2pi
+
+        sine[i] = sin(radians) * 0.5  + 0.5; // augment so from 0 to 1 duty cycle
+        radians += 0.1256637;
+    }
+
+    // Create triangle duty cycle with 50 samples for up and down each
+    // should go from 0 to 1 to 0. 1/50 = 0.02
+
+    float triangle_duty_cycle = 0;
+    for(int i = 0; i < 50; i++)
+    {
+        triangle[i] = triangle_duty_cycle;
+        triangle_duty_cycle += 0.02;
+    }
+
+    for(int i = 50; i < 100; i++)
+    {
+        triangle[i] = triangle_duty_cycle;
+        triangle_duty_cycle -= 0.02;
+    }
 }
 
 int main()
 {
     stdio_init_all();
+    generateWaveforms();
 
     // SPI initialisation. This example will use SPI at 1MHz.
     spi_init(SPI_PORT, 1000); //*1000);
@@ -60,12 +96,16 @@ int main()
 
     while (true) {
         printf("Hello, world!\n");
-        sleep_ms(1000);
-        
-        writeDAC(1, 3.3);
-        // for(int = 0; i < 100; i++) {
+
+        for(int i = 0; i < 100; i++) // one of these per second
+        {
             
-        // }
+
+            writeDAC(0, sine[(i % 50)]); // Use modulo to get the right sine value
+            writeDAC(1, triangle[i]); // Channel 0 for sine, 1 for triangle
+
+            sleep_ms(10);
+        }
     }
 
     
